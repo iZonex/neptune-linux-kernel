c311dd2b786f1fcb1c42f765da197ef4eec5991d
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2023-10-12 15:18:50 -0400

    ASoC: SOF: make .remove callback return void
    We don't use the returned value and return 0 anyways, let's follow the
    example of platform drivers and simplify the definitions.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Daniel Baluta <daniel.baluta@nxp.com>
    Reviewed-by: Rander Wang <rander.wang@intel.com>
    Reviewed-by: Péter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Link: https://lore.kernel.org/r/20231012191850.147140-5-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit e4d09de3919bb0ed5327acb238e849f3287f2706)
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- sound/soc/sof/intel/hda.c
+++ sound/soc/sof/intel/hda.c
@@ -1327,7 +1327,7 @@ hdac_bus_unmap:
 	return ret;
 }
 
-int hda_dsp_remove(struct snd_sof_dev *sdev)
+void hda_dsp_remove(struct snd_sof_dev *sdev)
 {
 	struct sof_intel_hda_dev *hda = sdev->pdata->hw_pdata;
 	const struct sof_intel_dsp_desc *chip = hda->desc;
@@ -1380,8 +1380,6 @@ skip_disable_dsp:
 
 	if (!sdev->dspless_mode_selected)
 		iounmap(sdev->bar[HDA_DSP_BAR]);
-
-	return 0;
 }
 
 int hda_dsp_remove_late(struct snd_sof_dev *sdev)
@@ -1389,8 +1387,6 @@ int hda_dsp_remove_late(struct snd_sof_dev *sdev)
 	iounmap(sof_to_bus(sdev)->remap_addr);
 	sof_hda_bus_exit(sdev);
 	hda_codec_i915_exit(sdev);
-
-	return 0;
 }
 
 int hda_power_down_dsp(struct snd_sof_dev *sdev)
