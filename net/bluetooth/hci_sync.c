85bf1a30bdb7bbfda78688f2de15ea6acbf8b2d1
zhongjun.yu <zhongjun.yu@quectel.com>
2023-12-22 11:17:51 +0800

    1) Fixed an issue where BLE devices could not turn wakeup on or off. 2) Resolved an issue where bluez lib could not receive MGMT_EV_DEVICE_FLAGS_CHANGED notifications after sending MGMT_OP_SET_DEVICE_FLAGS
    <Change Type>: Develop the function to set the Bluetooth device wake-up switch
    
    <<< Test Notes >>>
    <Test-Proposal>:
    1) python script set_wakeup.py
    2) valve sw guys Developing UI validation
    <Stress-Test>: Y

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -2399,6 +2399,7 @@ static u8 hci_update_accept_list_sync(struct hci_dev *hdev)
 	struct bdaddr_list *b, *t;
 	u8 num_entries = 0;
 	bool pend_conn, pend_report;
+	struct hci_conn_params *conn_params;
 	u8 filter_policy;
 	int err;
 
@@ -2444,6 +2445,15 @@ static u8 hci_update_accept_list_sync(struct hci_dev *hdev)
 		 * remove it from the acceptlist.
 		 */
 		if (!pend_conn && !pend_report) {
+			hci_le_del_accept_list_sync(hdev, &b->bdaddr,
+						    b->bdaddr_type);
+			continue;
+		}
+		
+		conn_params = hci_conn_params_lookup(hdev,&b->bdaddr,b->bdaddr_type);
+		/* During suspend, only wakeable devices can be in acceptlist */
+		if (conn_params && hdev->suspended &&
+		    !(conn_params->flags & HCI_CONN_FLAG_REMOTE_WAKEUP)) {
 			hci_le_del_accept_list_sync(hdev, &b->bdaddr,
 						    b->bdaddr_type);
 			continue;
