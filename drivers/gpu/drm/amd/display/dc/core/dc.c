f35692f353ea3c8674e3e0af7935d9054cae4250
Melissa Wen <melissa.srw@gmail.com>
2024-10-22 17:50:03 -0100

    Revert "[FROM-ML] drm/amd/display: Clear update flags after update has been applied"
    This reverts commit 3f5a64e93bcddc6b915aa8dcf35ec556c74222ae.

--- drivers/gpu/drm/amd/display/dc/core/dc.c
+++ drivers/gpu/drm/amd/display/dc/core/dc.c
@@ -4907,26 +4907,11 @@ static bool update_planes_and_stream_v3(struct dc *dc,
 	return true;
 }
 
-static void clear_update_flags(struct dc_surface_update *srf_updates,
-	int surface_count, struct dc_stream_state *stream)
-{
-	int i;
-
-	if (stream)
-		stream->update_flags.raw = 0;
-
-	for (i = 0; i < surface_count; i++)
-		if (srf_updates[i].surface)
-			srf_updates[i].surface->update_flags.raw = 0;
-}
-
 bool dc_update_planes_and_stream(struct dc *dc,
 		struct dc_surface_update *srf_updates, int surface_count,
 		struct dc_stream_state *stream,
 		struct dc_stream_update *stream_update)
 {
-	bool ret = false;
-
 	dc_exit_ips_for_hw_access(dc);
 	/*
 	 * update planes and stream version 3 separates FULL and FAST updates
@@ -4943,17 +4928,10 @@ bool dc_update_planes_and_stream(struct dc *dc,
 	 * features as they are now transparent to the new sequence.
 	 */
 	if (dc->ctx->dce_version > DCN_VERSION_3_51)
-		ret = update_planes_and_stream_v3(dc, srf_updates,
-						  surface_count,
-						  stream, stream_update);
-	else
-		ret = update_planes_and_stream_v2(dc, srf_updates,
-						  surface_count, stream,
-						  stream_update);
-	if (ret)
-		clear_update_flags(srf_updates, surface_count, stream);
-
-	return ret;
+		return update_planes_and_stream_v3(dc, srf_updates,
+				surface_count, stream, stream_update);
+	return update_planes_and_stream_v2(dc, srf_updates,
+			surface_count, stream, stream_update);
 }
 
 void dc_commit_updates_for_stream(struct dc *dc,
@@ -4963,8 +4941,6 @@ void dc_commit_updates_for_stream(struct dc *dc,
 		struct dc_stream_update *stream_update,
 		struct dc_state *state)
 {
-	bool ret = false;
-
 	dc_exit_ips_for_hw_access(dc);
 	/* TODO: Since change commit sequence can have a huge impact,
 	 * we decided to only enable it for DCN3x. However, as soon as
@@ -4972,22 +4948,17 @@ void dc_commit_updates_for_stream(struct dc *dc,
 	 * the new sequence for all ASICs.
 	 */
 	if (dc->ctx->dce_version > DCN_VERSION_3_51) {
-		ret = update_planes_and_stream_v3(dc, srf_updates,
-						  surface_count,
-						  stream, stream_update);
+		update_planes_and_stream_v3(dc, srf_updates, surface_count,
+				stream, stream_update);
+		return;
 	}
-	else if (dc->ctx->dce_version >= DCN_VERSION_3_2) {
-		ret = update_planes_and_stream_v2(dc, srf_updates,
-						  surface_count,
-						  stream, stream_update);
-	} else {
-		ret = update_planes_and_stream_v1(dc, srf_updates,
-						  surface_count, stream,
-						  stream_update, state);
+	if (dc->ctx->dce_version >= DCN_VERSION_3_2) {
+		update_planes_and_stream_v2(dc, srf_updates, surface_count,
+				stream, stream_update);
+		return;
 	}
-
-	if (ret)
-		clear_update_flags(srf_updates, surface_count, stream);
+	update_planes_and_stream_v1(dc, srf_updates, surface_count, stream,
+			stream_update, state);
 }
 
 uint8_t dc_get_current_stream_count(struct dc *dc)
